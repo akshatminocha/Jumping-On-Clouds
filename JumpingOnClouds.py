6eea7948e303d1baa1e78fa4e84cbf03f40ce28d
akshatminocha <akshatminocha@gmail.com>
2019-02-22 01:36:22 +0530

    first commit

---
+++ JumpingOnClouds.py
@@ -0,0 +1,16 @@
+def jumpingOnClouds(c):
+    jumps=0
+    i=0
+    while (i<len(c)-1):
+        if((i+2)<len(c) and c[i+2]==0):
+            i+=2
+            jumps+=1
+        else:
+            i+=1
+            jumps+=1
+    return(jumps)                
+
+n = int(input())
+c = list(map(int, input().rstrip().split()))
+result = jumpingOnClouds(c)
+print(result)
